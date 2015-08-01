4a844f4d6efbf72a87f0803f3987b42179ac6d03
Sean <sturner@nmt.edu>
2015-08-01 13:13:26 -0600

    make it not choke on whitespace

--- ch02/simple-parser.hs
+++ ch02/simple-parser.hs
@@ -4,11 +4,14 @@ import           Text.Parsec        hiding (spaces)
 -- from Hugo:
 type Parser a = Parsec String () a
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
