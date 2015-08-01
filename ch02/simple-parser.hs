c5d889593a8d447206a35c641aaf2cf1eda6ba40
Sean <sturner@nmt.edu>
2015-08-01 10:44:44 -0600

    add the simple barebones parser from ch02

---
+++ ch02/simple-parser.hs
@@ -0,0 +1,18 @@
+import           System.Environment
+import           Text.Parsec        hiding (spaces)
+
+-- from Hugo:
+type Parser a = Parsec String () a
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
+
+main :: IO ()
+main = do
+  args <- getArgs
+  putStrLn (readExpr (args !! 0))
