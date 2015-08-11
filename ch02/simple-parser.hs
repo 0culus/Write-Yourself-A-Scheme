add5513e5534459fe72c3fb750dcdc95a779cbe3
Sean <sturner@nmt.edu>
2015-08-11 00:36:05 -0600

    continue with boilerplate from chapter

--- ch02/simple-parser.hs
+++ ch02/simple-parser.hs
@@ -22,6 +22,23 @@ readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+            "#t" -> Bool True
+            "#f" -> Bool False
+            _    -> Atom atom
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
 main :: IO ()
 main = do
   args <- getArgs
